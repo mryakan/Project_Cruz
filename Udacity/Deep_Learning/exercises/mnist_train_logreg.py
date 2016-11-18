2b961361e23a73f2ed15b974c323b7e2e7dd22b6
Medhat R. Yakan <mryakan@gmail.com>
2016-11-17 19:17:51 -0500

    Add logistric regression training code

--- Udacity/Deep_Learning/exercises/mnist_train_logreg.py
+++ Udacity/Deep_Learning/exercises/mnist_train_logreg.py
@@ -40,6 +40,47 @@ Train a logistic regression classifier on the mnist data to solve Problem 6 stat
 import os
 import sys
 import pickle
+from sklearn.linear_model import LogisticRegressionCV, LogisticRegression
+from sklearn.model_selection import cross_val_score
+
+def create_logreg(cross_validate=True):
+    """Create a logistic regression classifier with or without Cross Validation"""
+    # For now, use defaults!
+    if cross_validate:
+        logreg = LogisticRegressionCV()
+    else:
+        logreg = LogisticRegression(verbose=0) # pylint: disable=R0204
+    return logreg
+
+def train_logreg(datasets, datasizes, use_cv):
+    """
+    Train the logistic regression classifier using specified dataset sizes from the training set in the given dataset
+    then check prediction scores
+    return list containing results and corresponding scores per datatset.
+    """
+    results = []
+    scores = []
+    maxsize = len(datasets['train_dataset'])
+    logreg = create_logreg(cross_validate=use_cv)
+    if not logreg:
+        print("ERROR: Unable to create Logistic Regression Classifier instance. Aborting!")
+        return None
+    for datasize in datasizes:
+        if datasize > maxsize:
+            print("ERROR: Specified datasize is too large (%s > %s)" % (datasize, maxsize))
+            return None
+        if datasize == 0:
+            datasize = maxsize
+        # Select and Flatten data
+        t_data = [d.flatten() for d in datasets['train_dataset'][:datasize]]
+        t_labels = datasets['train_labels'][:datasize]
+        print("Fitting using training data (dataset size=%s)..." % datasize)
+        result = logreg .fit(t_data, t_labels)
+        results.append(result)
+        print("Calculating cross validation scores on training data...")
+        score = cross_val_score(result, t_data, t_labels)
+        scores.append(score)
+    return result, scores
 
 def load_dataset(pickle_file):
     """Load the data"""
@@ -48,13 +89,13 @@ def load_dataset(pickle_file):
             datasets = pickle.load(f)
             f.close()
     except IOError as e:
-        print('Unable to open file', pickle_file, ':', e)
+        print('ERROR: Unable to open file', pickle_file, ':', e)
         return None
     except EOFError as e:
-        print('Unable to read data from', pickle_file, ':', e)
+        print('ERROR: Unable to read data from', pickle_file, ':', e)
         return None
     except pickle.UnpicklingError as e:
-        print('Unable to unpickle data from', pickle_file, ':', e)
+        print('ERROR: Unable to unpickle data from', pickle_file, ':', e)
         return None
     return datasets
 
@@ -77,7 +118,7 @@ def main():
     print("Trying to load regular dataset from pickle file", reg_file, '...')
     reg_data = load_dataset(reg_file)
     if not reg_data:
-        print("Unable to load regular dataset. Aborting!")
+        print("ERROR: Unable to load regular dataset. Aborting!")
         return False
     print("Loaded regular data.")
     #print('label', reg_data['train_labels'][1], ':\n', reg_data['train_dataset'][1])
@@ -85,10 +126,21 @@ def main():
     san_data = load_dataset(san_file)
     print("Trying to load sanitized dataset from pickle file", reg_file, '...')
     if not san_data:
-        print("Unable to load sanitized dataset. Skipping it!")
+        print("ERROR: Unable to load sanitized dataset. Skipping it!")
     else:
         print("Loaded Sanitized data.")
         #print('label', san_data['train_labels'][1], ':\n', san_data['train_dataset'][1])
+    datasizes = [50, 100, 1000, 5000]
+    try_all = False
+    if try_all:
+        datasizes.append(0) # (0 ==> all: May take a LONG time to complete)
+    # Train regular data with specified dataset sizes
+    for datasel in [(reg_data, "original data"), (san_data, "sanitized data")]:
+        for cv in [False, True]:
+            print("Training Logistic Regression classifier on %s (CV=%s)..." % (datasel[1], cv))
+            _, scores = train_logreg(datasel[0], datasizes, cv)
+            for i, datasize in enumerate(datasizes):
+                print("Mean accuracy for %s using %s samples: %s" % (datasel[1], datasize, scores[i].mean()))
     return True
 
 if __name__ == '__main__':
